a32d6065b51185a1c841473c38a1e995f9c4afb9
Mohammad Adji Febriansyah <72957851+MohammadAdjiFebriansyah@users.noreply.github.com>
2022-04-22 21:40:04 +0700

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,14 +2,12 @@ import { useDispatch } from 'react-redux';
 import { logout } from '../../TokenSlice/index';
 import Button from '../Button';
 import './index.css';
-import SpotifyLogo from '../../assets/SpotifyLogo.png';
 
 function Navbar() {
   const dispatch = useDispatch();
   return (
     <nav className='navbar'>
       <div className='navbarLogo'>
-        <img src={SpotifyLogo} alt='t' />
         <p>Spotify.</p>
       </div>
       <div className='navbarMenu'>
